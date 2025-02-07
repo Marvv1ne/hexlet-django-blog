ca7a01156a45aa785f82075e5a9d8ddf23fcf2a8
Marvv1ne <gorn89@inbox.ru>
2025-02-07 22:43:09 +0400

    feat: add index.html, about.html and make views index and about

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from hexlet_django_blog import views
 
 urlpatterns = [
+    path('', views.index),
+    path('about/', views.about), # <- добавляем эту строчку
     path('admin/', admin.site.urls),
 ]
